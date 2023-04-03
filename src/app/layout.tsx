679e0e3d29301fa6191c83b32eee96f7242a8acc
Cody Burley <codyburley@gmail.com>
2023-04-03 15:49:55 -0400

    added mobile menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/ultis";
 import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/Toast";
+import MobileMenu from "@/components/MobileMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +20,14 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
-          {children}
-          <Toaster position="bottom-right" />
           {/* @ts-expect-error Server Component */}
           <Navbar />
+          <Toaster position="bottom-right" />
+          <MobileMenu />
+          <main>{children}</main>
         </Providers>
-        {/* Allow for more height on mobile devices */}
+
+        {/* Allow more height for mobile menu on mobile */}
         <div className="h-40 md:hidden" />
       </body>
     </html>
